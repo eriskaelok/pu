1f1e0745e6dc1d26d80d6c1f3f839aa6b96d84fd
eriskaelok <elokjember8@gmail.com>
2022-04-22 14:58:34 +0700

    elok

---
+++ admin/adm_home.php
@@ -0,0 +1,21 @@
+<?php
+  include '../template/adm_nav.php';
+?>
+
+<div class="container">
+  <h3>Selamat datang ADMIN di Sistem Informasi Mahasiswa (SIMas)</h3>
+  <p>Ini adalah halaman informasi bagi mahasiswa baru di Universitas Merdeka Malang.</p>
+  <hr>
+
+  <h4>Peraturan ADMIN </h4>
+  <ol>
+    <li>Dilarang memeberikan akses Username dan Password pada siapapun.</li>
+    <li>Wajib melakukan checking pedataan Mahasiswa Baru.</li>
+    <li>Wajib melakukan update Daftar Mata Kuliah.</li>
+    <li>Wajib Melakukan update Daftar Dosen Pengajar.</li>
+  </ol>
+</div>
+
+<?php
+    include '../template/footer.php';
+?>
---
+++ mhs/mhs_home.php
@@ -0,0 +1,23 @@
+<?php
+    include '../template/mhs_nav.php';
+?>
+
+<div class="container">
+  <h3>Selamat datang di sistem MAHASISWA UNMER</h3>
+  <p>Ini adalah halaman informasi bagi mahasiswa baru di Universitas Merdeka Malang.</p>
+  <hr>
+
+  <h4>Peraturan Mahasiswa Baru</h4>
+  <ol>
+    <li>Menggunakan pakaian sopan berkemeja dan berkerah.</li>
+    <li>Menggunakan celana kain panjang.</li>
+    <li>Tidak menggunakan perhiasan berlebihan.</li>
+    <li>Dilarang membawa senjata tajam, narkotika, atau zat aditif lainnya.</li>
+  </ol>
+  <br>
+  
+</div>
+
+<?php
+    include '../template/footer.php';
+?>
